8507c0fe077ff41b54e195be61939b7f8d7d8248
Marco Carletti <mcarlett@redhat.com>
2025-03-05 11:02:53 +0100

    infinispan: updates and improvements

--- infinispan/src/main/java/org/apache/camel/example/springboot/infinispan/CamelInfinispanRoute.java
+++ infinispan/src/main/java/org/apache/camel/example/springboot/infinispan/CamelInfinispanRoute.java
@@ -20,7 +20,20 @@ import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.infinispan.InfinispanConstants;
 import org.apache.camel.component.infinispan.InfinispanOperation;
 
+import org.apache.camel.component.infinispan.remote.InfinispanRemoteEventListener;
+import org.apache.camel.impl.engine.DefaultShutdownStrategy;
+import org.apache.camel.model.rest.ParamDefinition;
+import org.apache.camel.model.rest.RestParamType;
+import org.infinispan.client.hotrod.event.ClientEvent;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Component;
+
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import static org.apache.camel.model.rest.RestParamType.path;
 
 /**
  * A simple Camel Infinispan route example using Spring-boot
@@ -29,19 +42,45 @@ import org.springframework.stereotype.Component;
 public class CamelInfinispanRoute extends RouteBuilder {
 
     @Override
-    public void configure() throws Exception {
+    public void configure() {
 
-        from("timer://foo?period=10000&repeatCount=1")
-            .to("direct:put-cache");
+        restConfiguration().inlineRoutes(false);
+
+        rest("/api/jdg")
+                .produces(MediaType.TEXT_PLAIN_VALUE)
+                .consumes(MediaType.TEXT_PLAIN_VALUE)
+                .post()
+                    .routeId("post-cache-item")
+                    .param()
+                        .name("id")
+                        .type(RestParamType.query)
+                        .dataType("string")
+                    .endParam()
+                .to("direct:put-cache");
 
         from("direct:put-cache")
+            .routeId("put-cache")
             .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.PUT)
-            .setHeader(InfinispanConstants.KEY).constant("1")
-            .setHeader(InfinispanConstants.VALUE).constant("test")
-            .to("infinispan://default")
+            .setHeader(InfinispanConstants.KEY).header("id")
+            .setHeader(InfinispanConstants.VALUE).body(String.class)
+            .log("PUT key : ${header.CamelInfinispanKey}, value : ${header.CamelInfinispanValue}")
+            .to("infinispan://{{cache.name}}");
+
+        from("infinispan://{{cache.name}}?eventTypes=CLIENT_CACHE_ENTRY_CREATED")
+            .routeId("listen-new-cache-item")
+            .log("Received headers: ${headers}")
+            .process(exchange -> {
+                Set<String> headers = new HashSet<>(exchange.getIn().getHeaders().keySet());
+                headers.stream().filter(key -> !key.equals(InfinispanConstants.KEY))
+                        .forEach(key -> exchange.getIn().removeHeader(key));
+            })
+            .to("direct:read-cache");
+
+        from("direct:read-cache")
+            .routeId("read-cache")
             .setHeader(InfinispanConstants.OPERATION).constant(InfinispanOperation.GET)
-            .setHeader(InfinispanConstants.KEY).constant("1")
-            .to("infinispan://default")
+            .log("GET key : ${header." + InfinispanConstants.KEY + "}")
+            .to("infinispan://{{cache.name}}")
             .log("Received body: ${body}");
     }
 
